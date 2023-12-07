a0ee287215ea8bf2e9fe16a535c891d1e11c02db
youssefabdelmaged <youssefabdelmaged50@1gmail.com>
2023-12-07 08:21:51 +0200

    5566

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,19 @@
+import Link from "next/link";
+
+import styles from "@/app/ui/Homepage.module.css";
+
 const Homepage = () => {
   return (
-    <div>Homepage</div>
-  )
-}
+    <div className={styles.bigContainer}>
+      <div className={styles.container}>
+        <h1 className={styles.h1}>Welcome</h1>
 
-export default Homepage
+        <Link href={"/login"}>
+          <button className={styles.button}>Login</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
